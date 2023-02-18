4c2d053e3945f62786372d7f17a237569fdda1fe
iceravenous <67291394+iceravenous@users.noreply.github.com>
2023-02-17 21:32:51 -0500

    v0217

--- src/com/coderscampus/assignment7/CustomArrayList.java
+++ src/com/coderscampus/assignment7/CustomArrayList.java
@@ -50,51 +50,59 @@ public class CustomArrayList<T> implements CustomList<T> {
 			Object temp[]= Arrays.copyOf(items, maxSize);
 			items = temp;
 		}
-		if (index == 0) {
-			Object temp[]= Arrays.copyOfRange(items, 0, maxSize-1);
-			for(int i=0; i<temp.length; i++) {
-				items[i+1] = temp[i];
+		
+			
+			if (index == 0) {
+				Object temp[]= Arrays.copyOfRange(items, 0, maxSize-1);
+				for(int i=0; i<temp.length; i++) {
+					items[i+1] = temp[i];
+				}
+				items[0]=item;
+				currentSize++;
+			} else {
+	//			Object temp1[] = Arrays.copyOfRange(items, 0, index-1);
+				Object temp2[] = Arrays.copyOfRange(items, index, maxSize);
+				items[index] = item;
+				int index2 = index+1;
+				for(int i=index2; i<maxSize; i++ ) {
+					items[i] = temp2[i-index2];
+				}
+				currentSize++;
 			}
-			items[0]=item;
-			currentSize++;
-		} else {
-//			Object temp1[] = Arrays.copyOfRange(items, 0, index-1);
-			Object temp2[] = Arrays.copyOfRange(items, index, maxSize);
-			items[index] = item;
-			int index2 = index+1;
-			for(int i=index2; i<maxSize; i++ ) {
-				items[i] = temp2[i-index2];
-			}
-			currentSize++;
-		}
+		
 		return true;
 		//return false;
 	}
 
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
-		if (index == 0) {
-			Object temp[]= Arrays.copyOfRange(items, 1, maxSize);
-			items=temp;
-			currentSize--;
-		} else if(index == currentSize) {
-			Object temp[] = Arrays.copyOfRange(items, 0, maxSize-1);
-			items = temp;
-			currentSize--;
+		System.out.println("CurrentSize: "+currentSize);
+		if (index > currentSize-1) {
+			throw new IndexOutOfBoundsException("Sorry, that index is invalid");
 		} else {
-			Object temp1[] = Arrays.copyOfRange(items, 0, index);
-			Object temp2[] = Arrays.copyOfRange(items, index+1, currentSize);
-			Object smaller[] = new Object[maxSize];
-			for(int i = 0; i<temp1.length; i++) {
-				smaller[i]=temp1[i];
+			if (index == 0) {
+				Object temp[]= Arrays.copyOfRange(items, 1, maxSize);
+				items=temp;
+				currentSize--;
+			} else if(index == currentSize) {
+				Object temp[] = Arrays.copyOfRange(items, 0, maxSize-1);
+				items = temp;
+				currentSize--;
+			} else {
+				Object temp1[] = Arrays.copyOfRange(items, 0, index);
+				Object temp2[] = Arrays.copyOfRange(items, index+1, currentSize);
+				Object smaller[] = new Object[maxSize];
+				for(int i = 0; i<temp1.length; i++) {
+					smaller[i]=temp1[i];
+				}
+				
+				for(int i = 0; i < temp2.length; ++i) {
+					smaller[temp1.length + i] = temp2[i];
+				}
+				items=smaller;
+				currentSize--;
+				
 			}
-			
-			for(int i = 0; i < temp2.length; ++i) {
-				smaller[temp1.length + i] = temp2[i];
-			}
-			items=smaller;
-			currentSize--;
-			
 		}
 		return null;
 	}
--- src/com/coderscampus/assignment7/ExampleApplication2.java
+++ src/com/coderscampus/assignment7/ExampleApplication2.java
@@ -8,16 +8,18 @@ public static void main(String[] args) {
 
 		
 		CustomList<String> newList = new CustomArrayList<>();
-		newList.add("Joshua");
-		newList.add("Matthias");
-		newList.add("Shaung");
-		newList.add("Robert");
-		newList.add("Sandy");
-		newList.add("Jonathon");
-		newList.add("Scout");
-		newList.add("Tiger");
-		newList.add("Keziah");
-		newList.add("Ben");
+		newList.add("01Joshua");
+		newList.add("02Matthias");
+		newList.add("03Shaung");
+		newList.add("04Robert");
+		newList.add("05Sandy");
+		newList.add("06Jonathon");
+		newList.add("07Scout");
+		newList.add("08Tiger");
+		newList.add("09Keziah");
+		newList.add("10Ben");
+//		newList.remove(9);
+//		newList.remove(9);
 //		newList.add("Joshua2");
 //		newList.add("Matthias2");
 //		newList.add("Shaung2");
@@ -28,12 +30,12 @@ public static void main(String[] args) {
 		
 
 		
-		System.out.println("Print list with index 0 removed and note size and that Joshua is gone");
-		newList.remove(0);
+//		System.out.println("Print list with index 0 removed and note size and that Joshua is gone");
+//		newList.remove(0);
 		for (int i=0; i<newList.getSize(); i++) {
 		    System.out.println(newList.get(i));
 		}
-		
+//		
 		
 		
 		
